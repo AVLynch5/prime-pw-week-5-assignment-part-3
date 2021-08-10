c63205cc3aefe59e4b72fe2fa10b40bf561af385
TJ Lynch <anthonyvlynch5@gmail.com>
2021-08-09 21:48:17 -0500

    Wk5-pt3 updated 3-music-collection with all required exercises

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -22,11 +22,27 @@ console.log('Adding album to the collection:', addToCollection('Discovery','Daft
 console.log(collection);
 
 function showCollection(array) {
-console.log('Number of albums in the collection is:', array.length);
-for (i=0; i<array.length; i++) {
-  console.log(array[i].title, 'by', array[i].artist + ', published in', array[i].yearPublished);
-}
+  console.log('Number of albums in the collection is:', array.length);
+  for (i=0; i<array.length; i++) {
+    console.log(array[i].title, 'by', array[i].artist + ', published in', array[i].yearPublished);
+  }
 }//end showCollection
 
 //Time to test showCollection
 showCollection(collection);
+
+function findByArtist(ArtistString) {
+  let resultsArr = [];
+  for (i=0; i<collection.length; i++) {
+    if (collection[i].artist === ArtistString) {
+      resultsArr.push(collection[i]);
+    }
+  }
+  return resultsArr;
+}//end findByArtist
+
+//Test findByArtist
+console.log('Artist search returned the following albums:', findByArtist('Kygo'));//expect 1 album object in results array
+console.log('Artist search returned the following albums:', findByArtist('Black Sabbath'));//expect empty results array
+console.log('Adding album to the collection:', addToCollection('Tron: Legacy','Daft Punk', '2010'));//adding a second album by Daft Punk to collection
+console.log('Artist search returned the following albums:', findByArtist('Daft Punk'));//expect 2 album objects in results array
