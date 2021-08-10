694603f46fab5c5f4e39f1add9b7898688206ae6
TJ Lynch <anthonyvlynch5@gmail.com>
2021-08-09 21:18:03 -0500

    Wk5-pt3 updated 3-music-collection with exercises up to showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,32 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(newAlbum);
+  return newAlbum;
+}//end addToCollection
+
+//Time to test addToCollection
+console.log('Adding album to the collection:', addToCollection('Appetite for Destruction','Guns N Roses', '1987'));
+console.log('Adding album to the collection:', addToCollection('Inside','Bo Burnham', '2021'));
+console.log('Adding album to the collection:', addToCollection('Cloud Nine','Kygo', '2016'));
+console.log('Adding album to the collection:', addToCollection('Aerosmith','Aerosmith', '1973'));
+console.log('Adding album to the collection:', addToCollection('Still in Love with You','Al Green', '1972'));
+console.log('Adding album to the collection:', addToCollection('Discovery','Daft Punk', '2001'));
+console.log(collection);
+
+function showCollection(array) {
+console.log('Number of albums in the collection is:', array.length);
+for (i=0; i<array.length; i++) {
+  console.log(array[i].title, 'by', array[i].artist + ', published in', array[i].yearPublished);
+}
+}//end showCollection
+
+//Time to test showCollection
+showCollection(collection);
